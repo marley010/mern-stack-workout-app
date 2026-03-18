50a61bc7fdc8b09611eb4cab9f46987e6a69d669
marley010 <9022034@student.zadkine.nl>
2026-03-18 10:43:45 +0100

    opdr 6

--- workouts/backend/src/routes/workoutRoutes.js
+++ workouts/backend/src/routes/workoutRoutes.js
@@ -1,43 +1,20 @@
-import Workout from '../models/Workout.js';
+// src/routes/workoutRoutes.js
 import express from 'express';
+import { 
+  getAllWorkouts, 
+  getWorkoutById, 
+  createWorkout 
+} from '../controllers/workoutController.js';
 
-// Maak router
 const router = express.Router();
 
 // GET alle workouts
-router.get('/', (req, res) => {
-  res.json({ message: 'GET alle workouts' });
-});
+router.get('/', getAllWorkouts);
 
 // GET één workout
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({ message: `GET workout ${id}` });
-});
+router.get('/:id', getWorkoutById);
 
 // POST nieuwe workout
-router.post('/', async (req, res) => {
-  const { title, load, reps } = req.body;
+router.post('/', createWorkout);
 
-  try {
-    const workout = await Workout.create({ title, reps, load });
-    res.status(201).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-// PATCH workout
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({ message: `PATCH workout ${id}` });
-});
-
-// DELETE workout
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({ message: `DELETE workout ${id}` });
-});
-
-// Exporteer router
 export default router;
